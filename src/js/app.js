639f41ff3ca07c9a52f3fe16403f55e7893cc2fa
Andrés <alderan133@gmail.com>
2021-06-21 20:15:04 +0200

    Segundo commit Barberia

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,51 @@
+let pagina=1;
+
 document.addEventListener('DOMContentLoaded', function(){
     iniciarApp();
 });
 
 function iniciarApp(){
     mostrarServicios();
+
+    //Resalta el DIV actual según el tab que se presiona
+    mostrarSeccion();
+
+    //Oculta o muestra una seccion según el tab que se presiona
+    cambiarSeccion();
+}
+
+function mostrarSeccion(){
+    const seccionActual =  document.querySelector(`#paso-${pagina}`);
+    seccionActual.classList.add('mostrar-seccion');
+
+    //Resalta el Tab actual
+    const tab= document.querySelector(`[data-paso="${pagina}"]`);
+    tab.classList.add('actual');
+}
+
+function cambiarSeccion(){
+    const enlaces =  document.querySelectorAll('.tabs button');
+    enlaces.forEach(enlace=>{
+        enlace.addEventListener('click', e=>{
+            e.preventDefault();
+
+            pagina= parseInt(e.target.dataset.paso);
+
+            //Eliminar mostrar-seccion de la seccion anterior
+            document.querySelector('.mostrar-seccion').classList.remove('mostrar-seccion');
+
+
+            //Agrega mostrar-seccion donde se hizo click
+            const seccion = document.querySelector(`#paso-${pagina}`);
+            seccion.classList.add('mostrar-seccion');
+
+            //Eliminar la clase actual en el tab anterior
+            document.querySelector('.tabs .actual').classList.remove('actual')
+            //Agregar la clase actual en el nuevo tab
+            const tab= document.querySelector(`[data-paso="${pagina}"]`);
+            tab.classList.add('actual');
+        })
+    })
 }
 
 async function mostrarServicios(){
